45facd7d65f88e77972cf53156e7d9d112fcd3cf
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-09-17 16:04:46 +0100

    Exercise 2.

--- exercise2.js
+++ exercise2.js
@@ -1,1 +1,19 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
+
+// In the body of the fetch, you are required to write 
+// the code that creates a new array (todoTitles) containing 
+// just the 
+// titles of all the todos and then displays the array.
+// Use the Array.map HOF in your solution. 
+
+.then(response => response.json())
+  .then(json => {
+     const list = json
+     const todoTitles = list.map(function(list) {
+        return list.title;
+     });
+     console.log(todoTitles)
+    })
+  .catch(function(err) { 
+    console.log(err);
+  });
