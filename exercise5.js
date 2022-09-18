7006f36caaf82bc66ac9d7b92bb9237ff33af8d2
SaoirseODonovan <74667820+SaoirseODonovan@users.noreply.github.com>
2022-09-18 16:03:01 +0100

    Exercise 5.

---
+++ exercise5.js
@@ -0,0 +1,31 @@
+// you are required to implement the exact requirement
+// as Exercise 3 BUT replace the map-filter 
+
+//ex.3
+// creates an array of uncompleted todo objects, 
+// where an object contains a 
+// userID and todo title.
+
+
+// combination with the reduce HOF.
+
+// Hint: The initial value for the reducer's accumulator will be an empty array ( [] ).
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(response => response.json())
+  .then(json => {
+
+     const completed2 = json.reduce((accumulator,todo) =>{ 
+        todo.completed
+        // using ? : conditional operator, shorthand for if-else statement.
+        ? accumulator
+        : accumulator.push({userId: todo.userId, title: todo.title})
+        return accumulator;
+        
+     },[]);
+     console.log(completed2)
+    })
+  
+  .catch(function(err) { 
+    console.log(err);
+  });
